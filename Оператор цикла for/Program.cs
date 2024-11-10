0e696f8be6ed91725ee4a274ec5889fed0a966d6
SHEL <SHEL@DESKTOP-6SFOS1M>
2024-11-10 17:49:56 +0200

    Добавьте файлы проекта.

---
+++ Оператор цикла for/Program.cs
@@ -0,0 +1,29 @@
+﻿try
+{
+    Console.Write("Введите k:");
+    int k = int.Parse(Console.ReadLine());
+    double s = 0;
+    for (int i = 1; i <= k; i++)
+    {
+        double t = Math.Pow(-1, i) * Math.Pow(i - 3, 2) / Factorial(i);
+        s += t;
+    }
+    Console.WriteLine($"Результат: {s:F2}");
+
+    double Factorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Factorial(n - 1);
+        }
+    }
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
